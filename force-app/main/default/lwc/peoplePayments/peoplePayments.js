e2e0753f5f69d0f2499bf88e3440dbeaafa31ff6
Tom Tobias <tom.tobias1@gmail.com>
2021-09-10 19:23:53 -0700

    Basic working do nothing component

---
+++ force-app/main/default/lwc/peoplePayments/peoplePayments.js
@@ -0,0 +1,10 @@
+import { LightningElement, wire } from 'lwc';
+//import getPeoplePayments from '@salesforce/apex/PeoplePayments.getPeoplePayments';
+
+export default class PeoplePayments extends LightningElement {
+    
+    //@wire(getPeoplePayments) 
+    //peoplePaymentsSummary;
+
+    
+}
