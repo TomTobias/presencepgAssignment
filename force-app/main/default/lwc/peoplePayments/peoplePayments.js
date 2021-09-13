925cd05d2da643157265c4dcf1e5685e65bd2b38
Tom Tobias <tom.tobias1@gmail.com>
2021-09-12 17:57:07 -0700

    refactor people payments to use wired method (in prep for refresh code)

--- force-app/main/default/lwc/peoplePayments/peoplePayments.js
+++ force-app/main/default/lwc/peoplePayments/peoplePayments.js
@@ -1,15 +1,30 @@
-import { LightningElement, wire } from 'lwc';
+import { LightningElement, wire, track } from 'lwc';
 //import getPeoplePayments from '@salesforce/apex/PeoplePayments.getPeoplePayments';
 import getPeoplePaymentSummaries from '@salesforce/apex/PaymentsService.getPeoplePaymentSummaries';
+import { refreshApex } from '@salesforce/apex';
 
 export default class PeoplePayments extends LightningElement {
 
-    
+    @track peoplePaymentSummaries = [];
+    wiredPeoplePaymentSummaries; // for apex refresh
 
     @wire(getPeoplePaymentSummaries) 
-    peoplePaymentSummaries;
+    peoplePaySum(result){
+        this.wiredPeoplePaymentSummaries = result
+        if(result.data) {
+            this.peoplePaymentSummaries = result.data
+            this.error = undefined
+        } else if (result.error){
+            this.error = result.error
+            this.peoplePaymentSummaries = [];
+        }
+    }
+    
+    //peoplePaymentSummaries;
 
-   
+    handleRefreshSummary() {
+
+    }
 
     
 }
